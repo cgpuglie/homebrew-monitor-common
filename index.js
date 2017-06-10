5ac0b6382a93f07cfca0db31f3f27145dd716b8b
cgpuglie <cgpuglie@asu.edu>
2017-06-10 14:01:51 -0700

    Adding authentication handler Validates authentication scheme and calls auth microservice to validate token.

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // libraries common to other microservices - will affect all newly built images
 const express = require('express')
 const chalk = require('chalk')
+const rp = require('request-promise')
+const authorization = require('auth-header')
 
 const middleware = express()
 
-module.exports = function common ({color, name, environment}) {
+module.exports = function common ({color, name, environment, authEndpoint}) {
   // do we log?
   const silent = (environment === 'Test')
 
@@ -35,10 +37,29 @@ module.exports = function common ({color, name, environment}) {
       : (!silent) ? console.log(`${!color ? name : chalk[color](name) } > ${err.message}`) : false // log only message - cleaner
   }
 
+  function authHandler({headers: { auth:authHeader }}, res, next) {
+    // validate auth endpoint provided, otherwise 500
+    authEndpoint || next({code: 500, err: new Error('No authentication endpoint defined!')})
+    const parsedHeader = authorization.parse(authHeader)
+    // check for correct scheme and validate with auth service
+    return parsedHeader.scheme === 'Bearer' || next({code: 400, err: new Error('Only Bearer authenication is accepted.')})
+      && rp({
+        method: 'POST',
+        uri: `${authEndpoint}/decode`,
+        resolveWithFullResponse: true,
+        json: {
+          token: parsedHeader.token
+        }
+      })
+      .then(res => res.statusCode === 200 ? next() : next({code: res.statusCode, err: new Error(res.status)}))
+      .catch(res => next({code: res.statusCode, err: new Error(res.status)}))
+  }
+
   
   // start server
   return {
     middleware,
-    errorHandler
+    errorHandler,
+    authHandler
   }
 }
