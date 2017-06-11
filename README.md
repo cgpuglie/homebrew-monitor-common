39bbc60531bbfaa50ad201028b0e25bc344fef3f
cgpuglie <cgpuglie@asu.edu>
2017-06-10 21:08:45 -0700

    Refactored, Implemented authHandler - Each handler is now a function, no express specific logic included - Implemented authenication handler, calls out to auth microservice

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Homebrew Monitor - Common
 
 Common logic for Homebrew-monitor microservices
+
+### Testing
+
+The easiest way to test the shared module is to mount the dependency into a microservice.
+```bash
+docker run \
+  -it --rm \
+  -v /home/cpuglies/projects/homebrew-monitor-common/:/usr/src/homebrew-monitor-rest/node_modules/homebrew-monitor-common/ \
+  homebrew-monitor-rest-test
+```
+
+For final testing, push to git repository and run end to end tests on entire stack.
--- index.js
+++ index.js
@@ -1,65 +1,64 @@
 // libraries common to other microservices - will affect all newly built images
-const express = require('express')
 const chalk = require('chalk')
 const rp = require('request-promise')
 const authorization = require('auth-header')
-
-const middleware = express()
 
 module.exports = function common ({color, name, environment, authEndpoint}) {
   // do we log?
   const silent = (environment === 'Test')
 
-  middleware.use('/health', function health (req, res) {
-    return res
-      .status(200)
-      .send({
-        ok: true
-      })
-  })
   // set 404 route
-  middleware.use(function notFound(req, res, next) {
+  function notFoundHandler(req, res, next) {
     return next({code: 404, err: new Error("Not Found")})
-  })
+  }
   // format normal error objects with status code
-  middleware.use(function error(error, req, res, next) {
+  function errorForwardHandler(error, req, res, next) {
     return error.code
     ? next(error)
     : next({code: 500, err: error})
-  })
+  }
   
   // create error handler function - doesn't work as middleware
   // log errors as applicable
   function errorHandler({code=500, err=new Error()}, req, res, next) {
     return res.status(code).send({message: err.message})
     && code >= 500
-      ? (!silent) ? console.log(`${!color ? name : chalk[color](name) } > ${err.message}`, err) : false // log error and stack trace above 500
-      : (!silent) ? console.log(`${!color ? name : chalk[color](name) } > ${err.message}`) : false // log only message - cleaner
+      ? console.log(`${!color ? name : chalk[color](name) } > ${err.message}`, err) // log error and stack trace above 500
+      : (!silent) ? console.log(`${!color ? name : chalk[color](name) } > ${err.message}`) : false // log only message unless silent - cleaner
   }
 
-  function authHandler({headers: { auth:authHeader }}, res, next) {
-    // validate auth endpoint provided, otherwise 500
-    authEndpoint || next({code: 500, err: new Error('No authentication endpoint defined!')})
-    const parsedHeader = authorization.parse(authHeader)
-    // check for correct scheme and validate with auth service
-    return parsedHeader.scheme === 'Bearer' || next({code: 400, err: new Error('Only Bearer authenication is accepted.')})
-      && rp({
-        method: 'POST',
-        uri: `${authEndpoint}/decode`,
-        resolveWithFullResponse: true,
-        json: {
-          token: parsedHeader.token
-        }
+  function healthHandler (req, res) {
+    return res
+      .status(200)
+      .send({
+        ok: true
       })
-      .then(res => res.statusCode === 200 ? next() : next({code: res.statusCode, err: new Error(res.status)}))
-      .catch(res => next({code: res.statusCode, err: new Error(res.status)}))
+  }
+  
+  function authHandler({headers: { auth:authHeader }}, res, next) {
+    return (authEndpoint || next({code: 500, err: new Error('No authentication endpoint defined!')})) // validate that an endpoint is defined
+    && (authHeader || next({code: 401, err: new Error('No authentication header provided.')}))
+    // check for correct scheme and validate with auth service
+    && (authorization.parse(authHeader).scheme === 'Bearer' || next({code: 400, err: new Error('Only Bearer authenication is accepted.')}))
+    && rp({
+      method: 'POST',
+      uri: `${authEndpoint}/decode`,
+      resolveWithFullResponse: true,
+      json: {
+        token: authorization.parse(authHeader)
+      }
+    })
+    .then(res => res.statusCode === 200 ? next() : next({code: res.statusCode, err: new Error(res.status)}))
+    .catch(res => next({code: res.statusCode, err: new Error(res.status)}))
   }
 
   
   // start server
   return {
-    middleware,
+    notFoundHandler,
+    errorForwardHandler,
     errorHandler,
-    authHandler
+    authHandler,
+    healthHandler
   }
 }
